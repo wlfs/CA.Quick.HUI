d4994fde698589b61b7b36a3a34e59eaeaa58edd
wl <wl@wl.com>
2016-07-28 18:17:44 +0800

    md5

--- CA.Quick/Utils/Helper.cs
+++ CA.Quick/Utils/Helper.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -29,6 +30,20 @@ namespace CA.Quick.Utils
                 str += pattern[patternIndex][rand.Next(len)];
             }
             return str;
+        }
+        public static string MD5(string str)
+        {
+            byte[] data = Encoding.GetEncoding("GB2312").GetBytes(str);
+            MD5 md5 = new MD5CryptoServiceProvider();
+            byte[] OutBytes = md5.ComputeHash(data);
+
+            string OutString = "";
+            for (int i = 0; i < OutBytes.Length; i++)
+            {
+                OutString += OutBytes[i].ToString("x2");
+            }
+            // return OutString.ToUpper();
+            return OutString.ToLower();
         }
     }
 }
